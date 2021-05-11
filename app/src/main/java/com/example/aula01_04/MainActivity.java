979b4563b25e025fbac1cbc36169f10e36357432
Thiago <thiago.nascimento99@hotmail.com>
2021-05-11 00:03:32 -0300

    First Commit

---
+++ app/src/main/java/com/example/aula01_04/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.aula01_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View v){
+        Toast.makeText(this, "Botão clicado diferente", Toast.LENGTH_SHORT).show();
+    }
+}
